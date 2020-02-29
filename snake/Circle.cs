7fa709f2e2ed6be8834fd6185373d2ccab001230
MilenaKolbina <milena.kolbina@gmail.com>
2020-02-29 23:04:17 +0300

    Initial commit

---
+++ snake/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Circle
+    {
+        public int X,Y;
+         public Circle ()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
